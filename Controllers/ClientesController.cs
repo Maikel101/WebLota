64b6d22167444d9de35a2a5d69b24049815ffcec
Maikel101 <miguel.rol@outlook.es>
2025-02-07 16:48:33 +0100

    Crear Clientes/Create/Succes

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebLota.Data;
+using WebLota.Models;
+
+namespace TuProyecto.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Nombre,Apellido,Email,Telefono,Mensaje")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                cliente.FechaRegistro = DateTime.Now;
+                _context.Add(cliente);
+                _context.SaveChanges();
+                TempData["Message"] = "El mensaje se ha enviado correctamente.";
+                return RedirectToAction("Success");
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/WebLota.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebLota.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebLota")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f28320124e44408d7e876a4cdc06f2e4063a617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606f0cf995271509a22f832ce8fcd386a5c07c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebLota")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebLota")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
